8e49414bb0d76a48f694363f224b14a7fe1b7cad
ashwin-athreya <85778918+ashwin-athreya@users.noreply.github.com>
2023-09-24 18:01:26 +0530

    Add files via upload

---
+++ Collection-Comparator-&-Comparable-Assignment/SortAccountBasedOnBalanceLowToHigh.java
@@ -0,0 +1,19 @@
+package p1;
+
+import java.util.Comparator;
+
+
+
+public class SortAccountBasedOnBalanceLowToHigh implements Comparator<Account>{
+
+ 
+
+	@Override
+
+	public int compare(Account account1, Account account2) {
+
+		return account1.getBalance()-account2.getBalance();
+
+	}
+
+}
